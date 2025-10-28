626df4b78d0294e75e63b7ecc6dea59261b9cfdb
Qwertuhh <arihantjain132407+github@gmail.com>
2025-10-28 21:24:57 +0530

    docs: Add note page with project status
    - Create a note page to inform users about the project status
    - Include links to the project's GitHub repository
    - Style the note with proper spacing and links

---
+++ src/app/note/page.tsx
@@ -0,0 +1,12 @@
+function Note() {
+    return (
+        <div className="flex flex-col items-left justify-center p-4">
+           <h1 className="text-2xl font-bold">Important</h1>
+           <p className="mt-2">This a discontinued project</p>
+           <p className="mt-2">If you still want to use it or contribute to it, please visit <a className="text-blue-500" href="https://github.com/isafruit">isafruit</a></p>
+           <p className="mt-2">For more information <a className="text-blue-500" href="https://github.com/isafruit">isafruit</a></p>
+        </div>
+    );
+}
+
+export default Note;
